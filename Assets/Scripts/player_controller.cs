4d394def3419a2f17ec565634a4d531bfb57235c
Benjamin Ledoux <bledoux2002@gmail.com>
2022-12-09 01:32:09 -0500

    modifications to player controller script, bug fixing

--- Assets/Scripts/player_controller.cs
+++ Assets/Scripts/player_controller.cs
@@ -3,44 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class player_controller : MonoBehaviour
 {
     public GameObject pauseMenu; //set pause menu
+    private bool isPaused = false;
 
-    private Rigidbody rb; //set rigidbody of player
-    private Transform movement; //set movement
+//    private Rigidbody rb; //set rigidbody of player
+    CharacterController charCtrl; //set character controller of player
+    Vector3 moveDir = Vector3.zero; //???
+    float rotationX = 0; //???
+//    private Transform movement; //set movement
 
-    public float speed; //determine speed of movement
+    public float walkSpeed = 7.0f; //determine speed of normal movement
+    public float sprintSpeed = 10.0f; //determine speed of sprinting
+    public float crouchSpeed = 4.0f; //determine speed of sneak movement
 
     public Camera cam; //set cam
-    public float sensitivity; //set sensitivity of camera turning
-    public float fov; //set field of view
+    public float sensitivity = 2.0f; //set sensitivity of camera turning
+    public float lookXLim = 45.0f; //set degree limit of camera turning
+    public float fov = 75.0f; //set field of view
 
-    private float rotateX; //set yaw
-    private float rotateY; //set pitch
+//    private float rotateX; //set yaw
+//    private float rotateY; //set pitch
 
-    public float gravScale; //set scale of gravity inflicted on Player
-    public float jumpForce; //set power of jump
-    public float distCheck; //how close character is to ground
-    private bool isGrounded; //determines if player on the ground
+    public float gravScale = 20.0f; //set scale of gravity inflicted on Player
+    public float jumpForce = 7.0f; //set power of jump
+//    public float distCheck; //how close character is to ground
+//    private bool isGrounded; //determines if player on the ground
 
     public int ammoCount; //hold # of ammo in inv
-    public float rateOfFire; //hold rate of fire in sec for weapon
+//    public float rateOfFire; //hold rate of fire in sec for weapon
     public Text ammoText; //display # of ammo in inventory
     public Text fireText; //appears when firing bullet
 
+    [HideInInspector]
+    public bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+//        rb = GetComponent<Rigidbody>();
+        charCtrl = GetComponent<CharacterController>();
+
+        // hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
         cam.fieldOfView = fov;
 
-        rotateX = 0.0f;
-        rotateY = 0.0f;
+//        rotateX = 0.0f;
+//        rotateY = 0.0f;
 
-//        jump = new Vector3(0.0f, 2.0f, 0.0f);
+//        jump = new Vector3(0.0f, 2.0f, 0.0f); 
 
         SetAmmoText();
         fireText.text = "";
@@ -49,7 +65,72 @@ public class player_controller : MonoBehaviour
     // FixedUpdate is called at a fixed interval, independent of frame rate
     void Update()
     {
-        // Move Player
+        if (!isPaused)
+        {
+            // ???
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            // Sprinting
+            //will have to make mroe complicated for crouch function, either swap ternary conditionals for if elif statements or more complex ternary conds
+            bool isSprinting = Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl);
+            float curSpeedX = canMove ? (isSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0; // bool ? if true (bool ? if true sprinting: if false walking) * W/S key : if false no movement
+            float curSpeedY = canMove ? (isSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0; // "ternary conditional operator ?:
+            float moveDirY = moveDir.y;
+            moveDir = (forward * curSpeedX) + (right * curSpeedY);
+
+            // Jumping
+            if (Input.GetButton("Jump") && canMove && charCtrl.isGrounded)
+            {
+                moveDir.y = jumpForce;
+            }
+            else
+            {
+                moveDir.y = moveDirY;
+            }
+
+            // Gravity
+            if (!charCtrl.isGrounded)
+            {
+                moveDir.y -= gravScale * Time.deltaTime;
+            }
+
+            // Movement
+            charCtrl.Move(moveDir * Time.deltaTime);
+
+            // Rotation
+            if (canMove)
+            {
+                rotationX += -Input.GetAxis("Mouse Y") * sensitivity;
+                rotationX = Mathf.Clamp(rotationX, -lookXLim, lookXLim); //Clamp prevents looking further than directly up or down
+                cam.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+            }
+
+            // Constantly check if Player fired weapon
+            StartCoroutine(ShootAmmo());
+
+            // Constantly check if Player is aiming down sights
+            StartCoroutine(AimDownSights());
+        }
+
+        // Pause Menu
+        if (Input.GetKeyDown("p")) //change to 'escape', doesn't work in editor
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            Cursor.visible = isPaused;
+        }
+
+/*        // Move Player
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
         //        float moveY = rb.velocity.y;
@@ -70,7 +151,7 @@ public class player_controller : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         } /* else {
             moveY = -0.375f;
-        } */ 
+        } * 
         Vector3 movement = new Vector3(moveX, 0.0f, moveZ) * speed;
         rb.AddForce(transform.TransformDirection(movement));
 
@@ -90,23 +171,7 @@ public class player_controller : MonoBehaviour
         cam.transform.eulerAngles = camRotation;
         Vector3 rotation = new Vector3(0, rotateY, 0);
         transform.eulerAngles = rotation;
-
-/*    }
-
-    // Update is called once per frame
-    void Update()
-    {*/
-        // Pause Menu
-        if (Input.GetKeyDown("p")) //change to 'escape', doesn't work in editor
-        {
-            pauseMenu.SetActive(!pauseMenu.activeSelf);
-        }
-
-        // Constantly check if Player fired weapon
-        StartCoroutine(ShootAmmo());
-
-        // Constantly check if Player is aiming down sights
-        StartCoroutine(AimDownSights());
+*/
     }
 
     // Event Trigger

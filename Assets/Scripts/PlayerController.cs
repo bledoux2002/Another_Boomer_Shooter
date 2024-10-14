8834bfcb7ce9ce44d428c14f406ccfa166537379
Benjamin Ledoux <bledoux2002@gmail.com>
2024-10-13 21:58:01 -0500

    pickup revamp
    pickup script now handles different pickups and collision
    weapon properly instantiates new weapon for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,34 +12,29 @@ public class PlayerController : MonoBehaviour
     public GameObject pauseMenu; //set pause menu
     private bool isPaused;
 
-//    private Rigidbody rb; //set rigidbody of player
     CharacterController charCtrl; //set character controller of player
     Vector3 moveDir = Vector3.zero; //???
     float rotationX = 0; //???
-//    private Transform movement; //set movement
 
     public float walkSpeed = 7.0f; //determine speed of normal movement
     public float sprintSpeed = 10.0f; //determine speed of sprinting
     public float crouchSpeed = 4.0f; //determine speed of sneak movement
+
+    public float gravScale = 20.0f; //set scale of gravity inflicted on Player
+    public float jumpForce = 7.0f; //set power of jump
 
     public Camera cam; //set cam
     public float sensitivity = 2.0f; //set sensitivity of camera turning
     public float lookXLim = 45.0f; //set degree limit of camera turning
     public float fov = 75.0f; //set field of view
 
-//    private float rotateX; //set yaw
-//    private float rotateY; //set pitch
-
-    public float gravScale = 20.0f; //set scale of gravity inflicted on Player
-    public float jumpForce = 7.0f; //set power of jump
-//    public float distCheck; //how close character is to ground
-//    private bool isGrounded; //determines if player on the ground
-
-    public int ammoCount; //hold # of ammo in inv
-//    public float rateOfFire; //hold rate of fire in sec for weapon
+    public int ammoCount = 0; //hold # of ammo in inv
+    private bool canFire = true; //if player can fire weapon
+    public float fireRate; //hold rate of fire in sec for weapon
     public Text ammoText; //display # of ammo in inventory
     public Text fireText; //appears when firing bullet
     private Vector3 fireTextPos;
+
     private System.Random rand;
 
     [HideInInspector]
@@ -49,7 +44,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         isPaused = GameObject.FindWithTag("canvas").GetComponent<PauseMenu>().GamePaused;
-        //        rb = GetComponent<Rigidbody>();
         charCtrl = GetComponent<CharacterController>();
 
         // hide cursor
@@ -58,14 +52,10 @@ public class PlayerController : MonoBehaviour
 
         cam.fieldOfView = fov;
 
-//        rotateX = 0.0f;
-//        rotateY = 0.0f;
-
-//        jump = new Vector3(0.0f, 2.0f, 0.0f); 
-
-        SetAmmoText();
+        UpdateAmmo(0);
         fireText.text = "";
         fireTextPos = fireText.transform.position;
+
         rand = new Random();
     }
 
@@ -116,7 +106,7 @@ public class PlayerController : MonoBehaviour
             }
 
             // Constantly check if Player fired weapon
-            StartCoroutine(ShootAmmo());
+            StartCoroutine(FireWeapon());
 
             // Constantly check if Player is aiming down sights
             StartCoroutine(AimDownSights());
@@ -138,97 +128,87 @@ public class PlayerController : MonoBehaviour
             Cursor.visible = isPaused;
         }
 
-// Old player controller scripting, before adding Character Controller Component. Can be removed but keeping temporarily to study for future reference
-/*        // Move Player
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveZ = Input.GetAxisRaw("Vertical");
-        //        float moveY = rb.velocity.y;
-        rb.AddForce(Physics.gravity * (gravScale - 1) * rb.mass);
+        // Old player controller scripting, before adding Character Controller Component. Can be removed but keeping temporarily to study for future reference
+        /*        // Move Player
+                float moveX = Input.GetAxisRaw("Horizontal");
+                float moveZ = Input.GetAxisRaw("Vertical");
+                //        float moveY = rb.velocity.y;
+                rb.AddForce(Physics.gravity * (gravScale - 1) * rb.mass);
 
-        // Check if player on ground, if so allow for jump, else they fall down
-        if (Physics.Raycast(transform.position, Vector3.down, distCheck))
-        {
-            isGrounded = true;
-            rb.drag = 1;
-        } else {
-            isGrounded = false;
-            rb.drag = 0;
-        }
-        if (isGrounded && Input.GetKey("space"))
-        {
-            //            moveY = jumpForce;
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        } /* else {
-            moveY = -0.375f;
-        } * 
-        Vector3 movement = new Vector3(moveX, 0.0f, moveZ) * speed;
-        rb.AddForce(transform.TransformDirection(movement));
+                // Check if player on ground, if so allow for jump, else they fall down
+                if (Physics.Raycast(transform.position, Vector3.down, distCheck))
+                {
+                    isGrounded = true;
+                    rb.drag = 1;
+                } else {
+                    isGrounded = false;
+                    rb.drag = 0;
+                }
+                if (isGrounded && Input.GetKey("space"))
+                {
+                    //            moveY = jumpForce;
+                    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                } /* else {
+                    moveY = -0.375f;
+                } * 
+                Vector3 movement = new Vector3(moveX, 0.0f, moveZ) * speed;
+                rb.AddForce(transform.TransformDirection(movement));
 
-        // Rotate Player
-        if ((-90 <= rotateX) && (rotateX <= 90))
-        {
-            rotateX = rotateX - (sensitivity * Input.GetAxis("Mouse Y"));
-        } else if (rotateX < -90)
-        {
-            rotateX = -90;
-        } else if (rotateX > 90)
-        {
-            rotateX = 90;
-        }
-        rotateY = rotateY + (sensitivity * Input.GetAxis("Mouse X"));
-        Vector3 camRotation = new Vector3(rotateX, rotateY, 0);
-        cam.transform.eulerAngles = camRotation;
-        Vector3 rotation = new Vector3(0, rotateY, 0);
-        transform.eulerAngles = rotation;
-*/
-    }
-
-    // Event Trigger
-    void OnTriggerEnter(Collider other)
-    {
-        // If hit pickup and space in inv, add to inv
-        if (other.gameObject.CompareTag("pickup") && ammoCount < 12)
-        {
-            // Remove old pickup and update UI
-            other.gameObject.SetActive(false);
-            ammoCount++;
-            SetAmmoText();
-
-            // Create new pickup in a random location in Test_Room
-
-        }
+                // Rotate Player
+                if ((-90 <= rotateX) && (rotateX <= 90))
+                {
+                    rotateX = rotateX - (sensitivity * Input.GetAxis("Mouse Y"));
+                } else if (rotateX < -90)
+                {
+                    rotateX = -90;
+                } else if (rotateX > 90)
+                {
+                    rotateX = 90;
+                }
+                rotateY = rotateY + (sensitivity * Input.GetAxis("Mouse X"));
+                Vector3 camRotation = new Vector3(rotateX, rotateY, 0);
+                cam.transform.eulerAngles = camRotation;
+                Vector3 rotation = new Vector3(0, rotateY, 0);
+                transform.eulerAngles = rotation;
+        */
     }
 
     // Update Ammo Count Text
-    void SetAmmoText()
+    public void UpdateAmmo(int num = -1)
     {
-        ammoText.text = ammoCount.ToString() + "/12";
+        ammoCount += num;
+        if (ammoCount > 100) ammoCount = 100;
+        ammoText.text = ammoCount.ToString() + " | 100";
     }
 
     // Fire one shot of ammo
-    IEnumerator ShootAmmo()
+    IEnumerator FireWeapon()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            /*
-            float timer = 0.0f;
-            timer = timer + Time.deltaTime;
-            if ((timer == 0) || (timer >= rateOfFire)) //not working
-            { */
-                if (ammoCount > 0)
-                {
-                    ammoCount--;
-                    SetAmmoText();
-                    //fireText.transform.position = new Vector3((float)rand.Next(-250, 250), (float)rand.Next(-100, 100), 0f);
-                    fireText.text = "pew"; // available ammo to be shot has been fired
-                } else {
-                    //fireText.transform.position = fireTextPos;
-                    fireText.text = "click"; // no available ammo to be shot
-                }
-                yield return new WaitForSeconds(1f); // leave effect on screen for 1 sec
-                fireText.text = "";
-            //}
+            if (ammoCount > 0)
+            {
+                canFire = false;
+                UpdateAmmo();
+                //fireText.transform.position = new Vector3((float)rand.Next(-250, 250), (float)rand.Next(-100, 100), 0f);
+                fireText.text = "pew"; // available ammo to be shot has been fired
+                StartCoroutine(FireRateHandler());
+            }
+            else
+            {
+                //fireText.transform.position = fireTextPos;
+                fireText.text = "click"; // no available ammo to be shot
+            }
+            yield return new WaitForSeconds(1f); // leave effect on screen for 1 sec
+            fireText.text = "";
         }
+    }
+
+    IEnumerator FireRateHandler()
+    {
+        float timeToFire = 1 / fireRate;
+        yield return new WaitForSeconds(timeToFire);
+        canFire = true;
     }
 
     // ADS
@@ -242,13 +222,14 @@ public class PlayerController : MonoBehaviour
                 cam.fieldOfView = fov - (fov * 0.05f * i); //float affects dist
                 yield return new WaitForSeconds(1 / 10);
             }
-        } else if (Input.GetMouseButtonUp(1)) {
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
             for (int i = 10; i > 0; i--)
             {
-                cam.fieldOfView = fov - (fov * 0.05f *i);
+                cam.fieldOfView = fov - (fov * 0.05f * i);
                 yield return new WaitForSeconds(1 / 10);
             }
         }
     }
-
 }
--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public PlayerController playerController;
+    public GameObject shotgun; //shotgun prefab for instantiation when picked up
+    public Vector3 shotgunPositionOffset = new Vector3(0, 1.5f, 0.5f);
+    public Vector3 shotgunRotationOffset = new Vector3(-90, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +23,31 @@ public class Pickup : MonoBehaviour
         
         // Object floats up and down
 
+    }
+
+    // Event Trigger, should prob move to scripts for each of these objects
+    void OnTriggerEnter(Collider playerCollider)
+    {
+        switch (gameObject.tag)
+        {
+            case "ammo":
+                if (playerController.ammoCount < 100)
+                {
+                    playerController.UpdateAmmo(10);
+                    gameObject.SetActive(false);
+                }
+                break;
+            case "weapon":
+                if (playerController.ammoCount < 100)
+                {
+                    playerController.UpdateAmmo(12);
+                    Transform camTransform = playerController.cam.transform;
+                    Vector3 shotgunPosition = camTransform.position + camTransform.TransformDirection(shotgunPositionOffset);
+                    Quaternion shotgunRotation = camTransform.rotation * Quaternion.Euler(shotgunRotationOffset);
+                    Instantiate(shotgun, shotgunPosition, shotgunRotation, camTransform);
+                    gameObject.SetActive(false);
+                }
+                break;
+        }
     }
 }
